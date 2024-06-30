482847bb62ca2d51f30309d69331c22377dcb6c4
bupd <bupdprasanth@gmail.com>
2024-06-30 23:14:54 +0530

    init day2 codes
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- 2015/day2.java
+++ 2015/day2.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class day2 {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class day2 {
+    //    2*l*w + 2*w*h + 2*h*l
+    public static void main(String[] args) {
+        ArrayList data = Input("2input");
+        System.out.println(data);
+//        System.out.println(solveCube(data) + "\nsolved");
+    }
+
+    private static String solveCube(String s) {
+
+        return "";
+    }
+
+    private static ArrayList<String[]> Input(String path) {
+        ArrayList<String[]> arr = new ArrayList<>();
+
+        try {
+            File myObj = new File(path);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data1 = myReader.nextLine();
+                String[] strArray = data1.split("x");
+                System.out.println(Arrays.toString(strArray));
+                arr.add(strArray);
+                System.out.println(Arrays.toString(arr));
+                arr.add(strArray);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return arr;
+    }
+
 }
